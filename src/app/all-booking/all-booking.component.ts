5bc30acabffae524459d622aab901cc5dd0f71e2
himenoworks <himeno.works001@gmail.com>
2022-06-15 15:30:46 +0700

    [Nu] add table all-booking

--- src/app/all-booking/all-booking.component.ts
+++ src/app/all-booking/all-booking.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core'
 import { BookingDetail } from '../booking'
 import { BookingService } from '../booking.service'
-declare const $: any
 
 @Component({
     selector: 'app-all-booking',
@@ -9,20 +8,25 @@ declare const $: any
     styleUrls: ['./all-booking.component.css'],
 })
 export class AllBookingComponent implements OnInit {
-    page = 1
-    pageSize = 10
 
     constructor(private bookingService: BookingService) {}
 
     bookings: BookingDetail[] = []
+    isLoading: boolean = false
+    listOfCurrentPageData: readonly BookingDetail[] = []
 
     ngOnInit(): void {
+        this.isLoading = true
         this.bookingService.getListBooking().subscribe((data) => {
+            this.isLoading = false
             this.bookings = data
         })
     }
+
+    onCurrentPageDataChange(listOfCurrentPageData: readonly BookingDetail[]): void {
+        this.listOfCurrentPageData = listOfCurrentPageData
+        console.log(listOfCurrentPageData)
+    }
 }
 
-export class NgbdPaginationBasic {
-
-}
+export class NgbdPaginationBasic {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,20 @@ import { HomeComponent } from './home/home.component'
 import { SidebarComponent } from './sidebar/sidebar.component'
 import { NavbarComponent } from './navbar/navbar.component'
 import { AllBookingComponent } from './all-booking/all-booking.component'
-import { HttpClientModule } from '@angular/common/http';
-
-//Table Module
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-
+import { NZ_I18N } from 'ng-zorro-antd/i18n'
+import { th_TH } from 'ng-zorro-antd/i18n'
+import { registerLocaleData } from '@angular/common'
+import th from '@angular/common/locales/th'
+import { NzTableModule } from 'ng-zorro-antd/table'
+registerLocaleData(th)
 
 @NgModule({
     declarations: [AppComponent, HomeComponent, SidebarComponent, NavbarComponent, AllBookingComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, NgbModule],
-    providers: [],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, NgbModule, FormsModule, NzTableModule],
+    providers: [{ provide: NZ_I18N, useValue: th_TH }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
