a08cd4f2a654f028894f33f8ffba7c6add538776
himenoworks <himeno.works001@gmail.com>
2022-06-21 09:38:44 +0700

    [Nu] merge navbar and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HomeComponent } from './home/home.component'
-import { SidebarComponent } from './sidebar/sidebar.component'
-import { NavbarComponent } from './navbar/navbar.component'
 import { AllBookingComponent } from './all-booking/all-booking.component'
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
@@ -17,7 +15,6 @@ import { MatSelectModule } from '@angular/material/select'
 import { NZ_I18N } from 'ng-zorro-antd/i18n'
 import { th_TH } from 'ng-zorro-antd/i18n'
 import { registerLocaleData } from '@angular/common'
-import { LoginComponent } from './login/login.component'
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
@@ -29,10 +26,7 @@ import { NavigationComponent } from './navigation/navigation.component';
     declarations: [
         AppComponent,
         HomeComponent,
-        SidebarComponent,
-        NavbarComponent,
         AllBookingComponent,
-        LoginComponent,
         NavigationComponent,
     ],
     imports: [
